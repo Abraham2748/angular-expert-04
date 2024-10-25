8f35095fc319a6907ca3fecd4f41be1c1307afd4
Abraham2748 <abraham_jack10@hotmail.com>
2024-10-24 21:22:53 -0500

    Avance clase 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +7,23 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-expert-04';
+
+  isRunningInBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.isRunningInBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngOnInit() {
+    if (this.isRunningInBrowser) {
+      console.log('Running in browser!');
+      localStorage.setItem('name', 'Angular Expert');
+    } else {
+      console.log('Running on server!');
+    }
+  }
 }
