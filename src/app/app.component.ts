530c9af7ac7f386e00377e13596283bc8c040ba3
Abraham2748 <abraham_jack10@hotmail.com>
2025-01-30 17:58:10 -0500

    ng update @angular/core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    imports: [],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-expert-04';
